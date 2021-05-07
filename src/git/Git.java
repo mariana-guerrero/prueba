c904c8b061924dc3a632b1c0a00dbc3c87b6f20e
MARIO.GUERRERO <MARIO.GUERRERO@CGITIC058687L.inegi.gob.mx>
2021-05-06 20:37:44 -0500

    este es un ejemplo

---
+++ src/git/Git.java
@@ -0,0 +1,15 @@
+
+package git;
+
+
+public class Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo ");
+    }
+    
+}
